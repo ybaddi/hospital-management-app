572fea67ac47b5453f5a650aaef7214090996c1f
baddi <baddi@MacBook-Pro-van-pc.local>
2022-07-20 12:18:25 +0100

    init the project with pom and src folder

---
+++ src/main/java/com/example/hospitalmanagementapp/HospitalManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.hospitalmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HospitalManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hospitalmanagementapp/HospitalManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitalmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
